91b4b4318e607f9a186323b7895f7842ae066545
gs0428 <lider28@naver.com>
2025-07-12 16:18:48 +0900

    chore: 초기 세팅

---
+++ app/routes/_index.tsx
@@ -0,0 +1,14 @@
+import HomePage from "~/pages/home";
+
+import type { Route } from "../+types/root";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return <HomePage />;
+}
---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import js from "@eslint/js";
+
+import { defineConfig } from "eslint/config";
+import pluginReact from "eslint-plugin-react";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    ignores: [".github", ".husky", "node_modules", ".react-router", "**/*.cjs"],
+  },
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            ["^\\u0000"],
+            ["^react$", ],
+            ["^@"],
+            ["^[a-z]"],
+            ["^~"],
+            ["^\\./", "^\\.\\./"],
+          ],
+        },
+      ],
+      "react/react-in-jsx-scope": "off",
+      "no-empty-pattern": "off",
+    },
+  },
+]);
