83354b21786ad9c371410cee791e70291790cea2
gs0428 <lider28@naver.com>
2025-08-27 15:53:30 +0900

    rename: 상담 신청 경로 변경

---
+++ app/pages/consult/apply/index.tsx
@@ -0,0 +1,3 @@
+export default function ConsultApplyPage() {
+  return <div>ConsultApplyPage</div>;
+}
--- app/routes/consult.apply.tsx
+++ app/routes/consult.apply.tsx
@@ -1,17 +1,17 @@
 import { WithBackHeader } from "~/components/layout/header/header";
 import PageLayout from "~/components/layout/page-layout";
-import ConsultingApplyPage from "~/pages/consulting/apply";
+import ConsultApplyPage from "~/pages/consult/apply";
 
-import type { Route } from "./+types/consulting.apply";
+import type { Route } from "./+types/consult.apply";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "상담 신청하기" }, { name: "description", content: "상담 신청하기" }];
 }
 
-export default function ConsultingApplyLayout() {
+export default function ConsultApplyLayout() {
   return (
     <PageLayout header={<WithBackHeader />}>
-      <ConsultingApplyPage />
+      <ConsultApplyPage />
     </PageLayout>
   );
 }
