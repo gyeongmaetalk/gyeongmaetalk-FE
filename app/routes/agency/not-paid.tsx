678d9297dd892a35891ff9fe7d90f955e29bf2f3
gs0428 <lider28@naver.com>
2025-08-29 17:09:48 +0900

    feat: 경매대행 초기 화면 퍼블리싱

---
+++ app/routes/agency/not-paid.tsx
@@ -0,0 +1,17 @@
+import ConsultantReviewCard from "~/components/card/consultant-review-card";
+import { Button } from "~/components/ui/button";
+
+export default function NotPaid() {
+  return (
+    <div className="space-y-4">
+      <div className="space-y-1">
+        <p className="text-label-strong font-headline2-bold">상담은 어떠셨나요?</p>
+        <p className="text-label-neutral font-body2-normal-regular">
+          경매톡과 함께 경매를 진행해보세요!
+        </p>
+      </div>
+      <ConsultantReviewCard />
+      <Button className="w-full">결제 후 대행 시작하기</Button>
+    </div>
+  );
+}
