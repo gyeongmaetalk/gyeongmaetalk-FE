c08a6c44d427a42b6a197b3ad3f3279e15bbc371
gs0428 <lider28@naver.com>
2025-09-02 08:31:49 +0900

    feat: 추천 매물이 없는 경우 페이지 퍼블리싱

--- app/pages/agency/recommend/index.tsx
+++ app/pages/agency/recommend/index.tsx
@@ -2,6 +2,7 @@ import { useSearchParams } from "react-router";
 
 import ConsultantReviewCard from "~/components/card/consultant-review-card";
 import Divider from "~/components/divider";
+import { Document } from "~/components/icons";
 import AgencyRecommendItem from "~/routes/agency.recommend._index/agency-recommend-item";
 import StatusNav from "~/routes/agency.recommend._index/status-nav";
 
@@ -10,16 +11,28 @@ const AgencyRecommendPage = () => {
   const status = searchParams.get("status") || "";
 
   return (
-    <div>
+    <div className="h-full">
       <section className="px-4 pt-6 pb-[18px]">
         <ConsultantReviewCard />
       </section>
       <StatusNav status={status} />
-      <section className="space-y-6 px-4 py-6">
+      {/* 추천 매물이 없는 경우 */}
+      <section className="mt-44 flex flex-col justify-center space-y-3">
+        <Document />
+        <div className="space-y-1 text-center">
+          <p className="font-headline2-bold text-label-strong">아직 추천매물이 없어요.</p>
+          <p className="font-body1-normal-regular text-label-neutral">
+            잠시만 기다려주세요.
+            <br />곧 맞춤 매물을 추천해드릴게요.
+          </p>
+        </div>
+      </section>
+      {/* 추천 매물이 있는 경우 */}
+      {/* <section className="space-y-6 px-4 py-6">
         <AgencyRecommendItem status="buy" />
         <Divider className="bg-cool-neutral-98" />
         <AgencyRecommendItem status="not-buy" />
-      </section>
+      </section> */}
     </div>
   );
 };
