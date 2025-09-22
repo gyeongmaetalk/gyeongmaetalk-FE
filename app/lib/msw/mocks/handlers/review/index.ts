1d2b5814e2ec2a4b6550f92b7c257f1c9da61cf7
gs0428 <lider28@naver.com>
2025-09-22 19:37:20 +0900

    feat: 리뷰 등록 MSW 세팅

--- app/lib/msw/mocks/handlers/review/index.ts
+++ app/lib/msw/mocks/handlers/review/index.ts
@@ -54,4 +54,60 @@ export const reviewHandlers = [
 
     return HttpResponse.json(baseReponse(review));
   }),
+  // 리뷰 생성
+  http.post("/reviews", async ({ request }) => {
+    const formData = await request.formData();
+    const body: { score: number; content: string; consultantId: string } = JSON.parse(
+      formData.get("request") as string
+    );
+
+    const createAt = new Date().toISOString();
+
+    const newId = allReviews.length + 1;
+    allReviews.push({
+      reviewId: newId,
+      name: "dkd1234",
+      createAt: createAt,
+      counselorName: "이정훈",
+      counselDateTime: createAt,
+      isMine: true,
+      score: body.score,
+      content: body.content,
+      imageCount: 0,
+      thumbnail: null,
+    });
+    consultantReviews.push({
+      reviewId: newId,
+      name: "dkd1234",
+      createAt: createAt,
+      isMine: true,
+      score: body.score,
+      content: body.content,
+      imageCount: 0,
+      thumbnail: null,
+    });
+    reviewDetails.push({
+      reviewId: newId,
+      name: "dkd1234",
+      createAt: createAt,
+      counselDateTime: createAt,
+      isMine: true,
+      score: body.score,
+      content: body.content,
+      images: [],
+      counselorName: "이정훈",
+      experience: 10,
+      counselorImage:
+        "https://i.namu.wiki/i/8mcZn4QTDZNSyG5LCLIltEOwSsrMoAG9TKsurgtD2zMPJWqQCYvZUsL_66BkJy3JmN4lhegQHg_A2iGdD-AWLw.webp",
+    });
+
+    // 2초 지연 후 응답
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    return HttpResponse.json(
+      baseReponse({
+        reviewId: newId,
+      })
+    );
+  }),
 ];
