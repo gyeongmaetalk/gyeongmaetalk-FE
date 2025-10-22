90268bd191e7e3b5017894221ed2903d61075f37
lucy <lucy726jeon@gmail.com>
2025-10-22 23:13:48 +0900

    feat: 사용자 정보 수정 기능 추가

--- app/services/auth.ts
+++ app/services/auth.ts
@@ -1,7 +1,14 @@
 import { api } from "~/lib/ky";
 import type { BaseResponse } from "~/models";
-import type { MyInfoResponse } from "~/models/auth";
+import type { MyInfoResponse, SignupResponse } from "~/models/auth";
+import type { UpdateUserInfoForm } from "~/routes/mypage.userinfo/schema";
 
 export const getMyInfo = async (): Promise<BaseResponse<MyInfoResponse>> => {
   return api.get("auth/info").json();
+};
+
+export const updateUserInfo = async (
+  data: UpdateUserInfoForm
+): Promise<BaseResponse<SignupResponse>> => {
+  return api.post("auth/signup", { json: data }).json();
 };
